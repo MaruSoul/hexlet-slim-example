0f1035471e699fa174944f47b647084beef48cb4
MarySoul <soulmeri0@gmail.com>
2021-10-21 17:22:41 +0300

    Веб-разработка на php. Поисковые формы

--- public/index.php
+++ public/index.php
@@ -1,19 +1,40 @@
 <?php
 
-
 // Подключение автозагрузки через composer
 require __DIR__ . '/../vendor/autoload.php';
 // Контейнеры в этом курсе не рассматриваются (это тема связанная с самим ООП), но если вам интересно, то посмотрите DI Container
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
 use DI\Container;
+
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
 
 $container = new Container();
 $container->set('renderer', function () {
     // Параметром передается базовая директория, в которой будут храниться шаблоны
-    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+    return new PhpRenderer(__DIR__ . '/../templates');
 });
+
+
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ($request, $response) {
+    return $this->get('renderer')->render($response, 'index.phtml');
+});
+
+$app->get('/users', function ($request, $response) use ($users) {
+    $term = $request->getQueryParam('term');
+    $filteredUsers = [];
+
+    foreach ($users as $user) {
+        if (str_contains($user, $term)) {
+            $filteredUsers[] = $user;
+        }
+    }
+    $params = ['users' => $filteredUsers, 'term' => $term];
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+});
 
 $app->get('/users/{id}', function ($request, $response, $args) {
     $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
